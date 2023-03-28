688ef101c75a65d9a229351a21f285cea4787a98
Aitov Ratmir <ratmirslv@gmail.com>
2023-03-28 13:21:15 +0300

    feat: add player

---
+++ components/VideoJS.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import videojs, { VideoJsPlayer, VideoJsPlayerOptions } from 'video.js'
+import 'video.js/dist/video-js.css'
+
+type VideoJsProps = {
+	options: VideoJsPlayerOptions
+	onReady?: (player: VideoJsPlayer) => null
+}
+export const VideoJS = ({ options, onReady }: VideoJsProps) => {
+	const videoRef = React.useRef<HTMLDivElement | null>(null)
+	const playerRef = React.useRef<VideoJsPlayer | null>(null)
+
+	React.useEffect(() => {
+		if (!playerRef.current) {
+			const videoElement = document.createElement('video-js')
+			videoElement.classList.add('vjs-big-play-centered')
+			videoRef.current?.appendChild(videoElement)
+			const player = (playerRef.current = videojs(videoElement, options, () => {
+				onReady && onReady(player)
+			}))
+		} else {
+			const player = playerRef.current
+
+			if (options.autoplay) {
+				player.autoplay(options.autoplay)
+			}
+			if (options.sources) {
+				player.src(options.sources)
+			}
+		}
+	}, [onReady, options, videoRef])
+
+	React.useEffect(() => {
+		const player = playerRef.current
+		return () => {
+			if (player && !player.isDisposed()) {
+				player.dispose()
+				playerRef.current = null
+			}
+		}
+	}, [playerRef])
+
+	return (
+		<div data-vjs-player>
+			<div ref={videoRef} />
+		</div>
+	)
+}
+
+export default VideoJS
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,41 @@
-import { Text, Title, Center, Container, Group } from '@mantine/core'
+import { Title, Center, SimpleGrid, Container } from '@mantine/core'
+import React, { useState } from 'react'
+import { VideoJsPlayerOptions } from 'video.js'
 
+import { VideoJS } from '@/components/VideoJS'
+
+const videoJsOptions: VideoJsPlayerOptions = {
+	autoplay: true,
+	controls: true,
+	responsive: true,
+	fluid: true,
+}
 export default function Media() {
+	const [srcVideo, setSrcVideo] = useState<string>()
+
 	return (
 		<Container>
-			<Center style={{ width: '60vw' }}>
-				<Group>
+			<SimpleGrid cols={1} spacing="sm">
+				<Center>
 					<Title order={1}>Media page</Title>
-					<Text>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum corrupti, ab
-						esse placeat corporis, quibusdam omnis sit id nam, totam voluptatum asperiores
-						cum perspiciatis! Quis, mollitia laboriosam fugiat incidunt obcaecati, enim
-						ratione commodi, rem libero inventore repellat! Voluptates a ex fugiat earum,
-						atque necessitatibus natus assumenda odio consequatur ut tenetur!
-					</Text>
-				</Group>
-			</Center>
+				</Center>
+				<input
+					type="file"
+					onChange={e => {
+						if (e.target.files !== null && e.target.files[0]) {
+							setSrcVideo(URL.createObjectURL(e.target.files[0]))
+						}
+					}}
+				/>
+				{srcVideo && (
+					<VideoJS
+						options={{
+							...videoJsOptions,
+							sources: [{ src: srcVideo, type: 'video/mp4' }],
+						}}
+					/>
+				)}
+			</SimpleGrid>
 		</Container>
 	)
 }
